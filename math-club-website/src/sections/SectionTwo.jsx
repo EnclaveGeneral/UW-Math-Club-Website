5c08a2cc84a781fd07585fa1800f832e61bd8860
EnclaveGeneral <zhangweiyebellevue@gmail.com>
2026-05-13 15:17:16 -0700

    V0.1.7.1 Integration Bee Displayed

--- math-club-website/src/sections/SectionTwo.jsx
+++ math-club-website/src/sections/SectionTwo.jsx
@@ -3,6 +3,10 @@ import { Grid, Typography } from "@mui/material";
 import event_1_pic from "../assets/example_event_1.jpg";
 import event_2_pic from "../assets/example_event_1.jpg";
 import event_3_pic from "../assets/example_event_1.jpg";
+import integration_bee_1 from "../assets/integration_bee_1.jpg";
+import integration_bee_2 from "../assets/integration_bee_2.jpg";
+import integration_bee_3 from "../assets/integration_bee_3.jpg";
+import integration_bee_4 from "../assets/integration_bee_4.jpg";
 
 export default function SectionTwo() {
     return (
@@ -23,19 +27,41 @@ export default function SectionTwo() {
             <Grid container spacing={2} justifyContent="center" alignItems={"center"} sx={{mt: {xs: 6, sm: 8, md: 10}}}>
                 <Grid size={4} textAlign={"center"}>
                     <img src={event_1_pic} style={{ width: "75%" }} />
-                    <Typography variant="h6" sx={{ fontSize: "clamp(0.25rem, 2.75vw, 2.5rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>Husky Math Talk Event</Typography>
+                    <Typography variant="h6" sx={{ fontSize: "clamp(0.15rem, 2.25vw, 2.0rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>Husky Math Talk Event</Typography>
                 </Grid>
 
                  <Grid size={4} textAlign={"center"}>
                     <img src={event_2_pic} style={{ width: "75%" }} />
-                    <Typography variant="h6" sx={{ fontSize: "clamp(0.25rem, 2.75vw, 2.5rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>Husky Estimathon Event</Typography>
+                    <Typography variant="h6" sx={{ fontSize: "clamp(0.15rem, 2.25vw, 2.0rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>Husky Estimathon Event</Typography>
                 </Grid>
 
                  <Grid size={4} textAlign={"center"}>
                     <img src={event_3_pic} style={{ width: "75%" }} />
-                    <Typography variant="h6" sx={{ fontSize: "clamp(0.25rem, 2.75vw, 2.5rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>Annual Integration Bee</Typography>
+                    <Typography variant="h6" sx={{ fontSize: "clamp(0.15rem, 2.25vw, 2.0rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>Annual Integration Bee</Typography>
                 </Grid>
 
+            </Grid>
+
+            <Typography variant="h5" sx={{ fontSize: "clamp(0.75rem, 2.75vw, 2.25rem)", color: "#ffffff", mt: {xs: 4, sm: 6, md: 8}}}>
+                Congratulations To Our Previous Integration Bee Winners!
+            </Typography>
+
+            <Grid container spacing={2} justifyContent="center" alignItems={"center"} sx={{mt: {xs: 6, sm: 8, md: 10}}}>
+
+                <Grid size={6} textAlign={"center"}>
+                    <img src={integration_bee_1} style={{ width: "75%" }} />
+                    <Typography variant="h6" sx={{ fontSize: "clamp(0.15rem, 2.25vw, 2.0rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>Our Winner</Typography>
+                </Grid>
+
+                
+
+                <Grid size={6} textAlign={"center"}>
+                    <img src={integration_bee_3} style={{ width: "75%" }} />
+                    <Typography variant="h6" sx={{ fontSize: "clamp(0.15rem, 2.25vw, 2.0rem)", color: "#ffffff", fontStyle: "italic", mt: {xs: 0.5, sm: 1.0, md: 1.5}}}>First + Second Place</Typography>
+                </Grid>
+
+                
+                
             </Grid>
 
         </Box>
