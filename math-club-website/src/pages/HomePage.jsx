808559a8050421af488480a7b9680a20e13ebdf8
EnclaveGeneral <zhangweiyebellevue@gmail.com>
2026-05-22 17:44:22 -0700

    V1.0.4 Home Page Change

--- math-club-website/src/pages/HomePage.jsx
+++ math-club-website/src/pages/HomePage.jsx
@@ -11,7 +11,6 @@ export default function HomePage() {
             <SectionOne />
             <SectionOneHalf />
             <SectionTwo />
-            <SectionThree />
             <SectionFour />
             <Modal />
         </div>
